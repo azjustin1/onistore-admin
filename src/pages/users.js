5e3c0b9c71b8d63ef6648724126b1c77b958087e
azjustin1 <azjustin3@gmail.com>
2021-03-28 17:37:10 +0700

    Initialze

---
+++ src/pages/users.js
@@ -0,0 +1,16 @@
+// in src/users.js
+import * as React from "react";
+import { List, Datagrid, TextField, EmailField } from "react-admin";
+
+export const UserList = (props) => (
+	<List {...props}>
+		<Datagrid rowClick="edit">
+			<TextField source="id" />
+			<TextField source="name" />
+			<TextField source="username" />
+			<EmailField source="email" />
+			<TextField source="address.street" />
+			<TextField source="phone" />
+		</Datagrid>
+	</List>
+);
